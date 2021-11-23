eecd2a48f6565369129017d98fb9c0ccdd178f22
Shanegggg <56031476+shanegoh@users.noreply.github.com>
2021-11-23 15:17:26 +0800

    Delete Ethereum-RingCT directory

--- Ethereum-RingCT/truffle/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var ringCT = artifacts.require("./ringCT.sol");
-var RingSig = artifacts.require("./RingSig.sol");
-module.exports = function(deployer) {
-  deployer.deploy(RingSig);
-  deployer.link(RingSig, ringCT);
-  deployer.deploy(ringCT);
-};
