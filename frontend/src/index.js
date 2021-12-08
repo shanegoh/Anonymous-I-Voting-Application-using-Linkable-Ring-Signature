14338cebe748ce0f60702d0a2a22fc977fd34fbf
Wen Han <shanegohwenhan@gmail.com>
2021-12-08 13:27:27 +0800

    updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
