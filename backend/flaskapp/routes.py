bf915c8f4528e77ae8f74efda7d2ab80011c5e99
Wen Han <shanegohwenhan@gmail.com>
2022-01-14 11:58:32 +0800

    asd

--- backend/flaskapp/routes.py
+++ backend/flaskapp/routes.py
@@ -20,7 +20,7 @@ def testRoute():
 @app.route("/test")
 @cross_origin(origin='localhost', headers=['Content-Type'])
 def test():
-   return os.getenv('SECRET_KEY')
+   return "ok"
 
 # This is used when user logs in for redirecting (For All Types of Users)
 @app.route("/findUserInformation")
