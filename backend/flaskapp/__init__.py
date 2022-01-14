9e7a7d087622e3eed2666eebdc7375eb0128266b
Wen Han <shanegohwenhan@gmail.com>
2022-01-14 11:57:01 +0800

    asd

--- backend/flaskapp/__init__.py
+++ backend/flaskapp/__init__.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 CORS(app)
 # app.config['SECRET_KEY'] = 'W/eh(9Rwku>yJp3YrihC!aKe[Ngu<`'
-#app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
