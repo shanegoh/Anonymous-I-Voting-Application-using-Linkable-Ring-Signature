9db4a465db360d5ace57e6406f49a8aba3fe99dc
Wen Han <shanegohwenhan@gmail.com>
2022-01-28 00:46:33 +0800

    asd

--- backend/flaskapp/db.py
+++ backend/flaskapp/db.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flaskapp.__init__ import *
+import os
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}?ssl_ca=./flaskapp/ap-southeast-1-bundle.pem'.format(os.getenv("MYSQL_DATABASE_USER"), os.getenv("MYSQL_DATABASE_PASSWORD"), os.getenv("MYSQL_DATABASE_HOST"), os.getenv("MYSQL_DATABASE_DB"))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://%s:%s@%s/%s?ssl_ca=./flaskapp/ap-southeast-1-bundle.pem' % (os.getenv("MYSQL_DATABASE_USER"), os.getenv("MYSQL_DATABASE_PASSWORD"), os.getenv("MYSQL_DATABASE_HOST"), os.getenv("MYSQL_DATABASE_DB"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
